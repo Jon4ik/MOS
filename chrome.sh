b3a8788022a35981af646709464f415e7b8b0c9a
Dmitry Nikishin <33792264+Jon4ik@users.noreply.github.com>
2023-06-05 08:15:43 +0300

    Update chrome.sh

--- chrome.sh
+++ chrome.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-###
-###	@script_author Dmitry Nikishin
-### https://t.me/Jon4ik
-### https://github.com/Jon4ik/MOS
-###
 
 if [ ! $(whoami) == 'root' ]; then
 	echo -e "\e[31mДанный скрипт должен быть запущен от root\e[0m"
     exit 1
 fi
 
-mkdir temp_chrome_install
+echo -e "\e[96mscript by Dmitry Nikishin"
+echo -e "\e[96mhttps://t.me/Jon4ik"
+echo -e "\e[96mhttps://github.com/Jon4ik/MOS"
 
-cd ~/temp_chrome_install
+for n in {3..0}; do
+  printf "\r\e[92mОжидание запуска скрипта <%s> " $n
+  sleep 1
+done
 
+mkdir /root/temp_chrome_install
 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm && dnf install ~/temp_chrome_install/google-chrome-stable_current_x86_64.rpm -y
+cd /root/temp_chrome_install
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm &> /dev/null && dnf install /root/temp_chrome_install/google-chrome-stable_current_x86_64.rpm -y &> /dev/null
 
 if [ id teacher &> /dev/null]; then
 	cd /home/teacher/'Рабочий стол' && ln -sf /usr/share/applications/google-chrome.desktop google-chrome.desktop && echo -e "\e[92mСоздан значек на рабочем столе Учителя\e[0m"
@@ -29,4 +32,4 @@ else
     exit 1
 fi
 
-rm -Rf ~/temp_chrome_install
+rm -Rf /root/temp_chrome_install
